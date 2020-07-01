a39f658856e77cec2816f3ea19b3dd2d4daead7b
Bill <williamspracklin@gmail.com>
2020-07-01 17:52:42 -0400

    Initial commit

---
+++ firstScript.js
@@ -0,0 +1,22 @@
+function firstFunction() {
+    alert("My first function!")
+}
+
+function sayHiTo(name) {
+    alert("Hi " + name)
+}
+
+function sayHiToTwoPeople(name1,name2) {
+    alert("Hi " + name1 + " and " + name2 + "!")
+}
+
+function addFiveTo(someNumber) {
+    return someNumber + 5
+}
+
+function addTenTo(someNumber) {}
+
+function sayHelloToUser() {
+    let name = document.getElementById("name").value;
+    alert("Hello " + name)
+}
